19ee26c797696ad4d7a878fe878ab48469be0f6f
Antonio C. Monroy <tony_monroe1212@hotmail.com>
2022-03-25 20:59:39 -0600

    Finished

--- main.cpp
+++ main.cpp
@@ -13,29 +13,8 @@ vector<string> COLORS = {
     "skyblue",
     "teal",
     "tomato",
-    "violet"
+    "violet",
 };
-
-void createLFile(vector<RegularExpression> reg_expressions){
-    int lineNum = 0;
-    
-    ifstream baseFile("./core/base.l");
-    ofstream targetFile("./core/lexic.l");
-    string reading_file;
-    while(getline(baseFile, reading_file)){
-        
-        if(lineNum <= 7){
-            targetFile << line;
-        } else {
-            
-            for(int i = 0; i < reg_expressions; i++){
-                targetFile << regex_vector[i].getRegexLine() << "\n"; 
-            }
-            
-        } 
-
-    }
-}
 
 class RegularExpression{
 
@@ -44,23 +23,38 @@ class RegularExpression{
         string type;
         string color;
         
-        RegularExpression(string _regex, string _type){
+        RegularExpression(string _regex, int _color_index){
             this->regex = _regex;
-            this->type = _type; 
-        }
-
-        RegularExpression(string _regex, string _type, int _color_index){
-            this->regex = _regex;
-            this->type = _type;
             this->color = COLORS[_color_index]; 
         }
-
+    
         string getRegexLine(){
-            return this->regex + " {fprintf(yyout, \"<span style=\"color: " + this->color + ";\">%s</span>\", yytext)}";
+            return this->regex + " {fprintf(yyout, \"<span style=\'color: " + this->color + ";\'>%s</span>\", yytext);}";
         }
-
-
 };
+
+void createLFile(vector<RegularExpression> reg_expressions){
+    int lineNum = 0;
+    
+    ifstream baseFile("./core/base.l");
+    ofstream targetFile("./core/lexic.l");
+    string reading_l;
+    bool flag = false;
+    while(getline(baseFile, reading_l)){
+        
+        targetFile << reading_l << "\n";
+
+        if(lineNum == 7){
+            cout << "in" <<endl;
+            for(int i = 0; i < reg_expressions.size(); i++){
+                targetFile << reg_expressions[i].getRegexLine() << "\n"; 
+            } 
+        }
+        
+        lineNum++;
+    }
+}
+
 vector<RegularExpression> getRegExpressions(string source_path) {
     
     vector<RegularExpression> output; 
@@ -69,11 +63,10 @@ vector<RegularExpression> getRegExpressions(string source_path) {
     string line, regex, type;
     int color_index = 0;
     while(getline(file, line)){
-        stringstream ss(line);
-        getline(ss, regex, ' ');
-        getline(ss, type, ' ');
-        cout<<regex<<" "<<type<<endl;
-        RegularExpression current_regex(regex,type,color_index);
+        // stringstream ss(line);
+        // getline(ss, regex, ' ');
+        // getline(ss, type, ' ');
+        RegularExpression current_regex(line,color_index);
         output.push_back(current_regex); 
         color_index++;
     }
@@ -81,8 +74,10 @@ vector<RegularExpression> getRegExpressions(string source_path) {
     return output;
 }
 
+    
 
-int main(int argc, char **argv){
+
+int main(){
 
     // if(argc < 2){
     //     cout << "File names required" << endl;
@@ -92,12 +87,11 @@ int main(int argc, char **argv){
     string regex_route = "regex.txt"; 
     
     vector<RegularExpression> regex_vector = getRegExpressions(regex_route);
-
-    for(int i = 0; i < regex_vector.size() ; i++) {
-        
-        cout << regex_vector[i].getRegexLine() << endl;
-    }
-
+    // cout << regex_vector.size()<< endl;
+    createLFile(regex_vector);
+    
+    system("flex core/lexic.l");
+    system("gcc core/Scanner.c -o highlighter");
 
     return 0;
 }
